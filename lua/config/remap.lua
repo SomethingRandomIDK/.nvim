5d8c28860340dc4edcf7bf75a6b1b0f715bf10d0
Mehul <mehtomar@gmail.com>
2024-09-05 10:08:45 -0400

    Added treesitter

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -3,3 +3,32 @@ vim.g.mapleader=" "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("i", "<C-c>", "<Esc>")
 vim.keymap.set("v", "<C-c>", "<Esc>")
+
+vim.opt.guicursor = ""
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.cursorline = true
+
+vim.opt.wrap = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.scrolloff = 8
+
+vim.opt.colorcolumn = "80"
+
+vim.api.nvim_set_option("clipboard", "unnamed")
+
+vim.g.netrw_banner = 0
+-- vim.g.netrw_liststyle = 3
+
