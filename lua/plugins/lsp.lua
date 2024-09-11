87ccdeab89ee4f835fc4ab16adde97dff089ff1a
Mehul <mehtomar@gmail.com>
2024-09-10 20:12:34 -0400

    Grammarly and tsserver wasn't working properly

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -32,9 +32,9 @@ return {
         local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
 
         local default_setup = function(server)
-          require('lspconfig')[server].setup({
-            capabilities = lsp_capabilities,
-          })
+            require('lspconfig')[server].setup({
+                capabilities = lsp_capabilities,
+            })
         end
 
         require('mason').setup({})
